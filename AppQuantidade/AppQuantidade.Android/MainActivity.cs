7ed189212442738d3b780e836d53458109186390
elielson <elielson@WS713>
2022-05-10 17:17:52 -0300

    Ajustes

--- AppQuantidade/AppQuantidade.Android/MainActivity.cs
+++ AppQuantidade/AppQuantidade.Android/MainActivity.cs
@@ -8,7 +8,7 @@ using Android.OS;
 
 namespace AppQuantidade.Droid
 {
-    [Activity(Label = "AppQuantidade", Icon = "@mipmap/ic_launcher", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Theme = "@style/MainTheme", MainLauncher = false, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
